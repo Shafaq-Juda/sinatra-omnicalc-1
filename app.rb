27e482f9c3f593c43c9e1e3caf29f6a062c345f7
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-10 05:59:48 +0000

    Calculate square running correctly

--- app.rb
+++ app.rb
@@ -7,3 +7,17 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/square/new")do
+erb(:square)
+end
+
+get("/square/results")do
+@num = params.fetch("a_num").to_i
+@square = @num ** 2.to_f
+erb(:square_results)
+end
