9b30ddf28f073fa4e844741e5e303e220a0c6dff
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-10 09:31:34 +0000

    payment working successfully

--- app.rb
+++ app.rb
@@ -17,7 +17,35 @@ erb(:square)
 end
 
 get("/square/results")do
-@num = params.fetch("a_num").to_i
+@num = params.fetch("a_num").to_f
 @square = @num ** 2.to_f
 erb(:square_results)
+end
+
+get("/square_root/new")do
+erb(:square_root)
+end
+
+get("/square_root/results")do
+@num = params.fetch("a_num").to_f
+@square_root = @num ** 0.5.to_f
+erb(:square_root_results)
+end
+
+get("/payment/new")do
+erb(:payment)
+end
+
+get("/payment/results")do
+@apr = params.fetch("apr").to_f
+@apr.to_fs(:percentage, { :precision => 4 })
+@r = (@apr / 100) / 12.to_f
+@years =  params.fetch("years").to_i
+@n = @years * 12.to_i
+@pv = params.fetch("principal").to_f
+@pv.to_fs(:currency)
+@numerator = (@r * @pv).to_f
+@denominator = 1 - (1+@r) ** -@n
+@p = (@numerator/@denominator).to_fs(:currency)
+erb(:payment_results)
 end
